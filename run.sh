62539fffc1809ca80a450cf35a8dd87d3a310df0
Ivan Kulikov <kulikovis91@gmail.com>
2024-03-23 18:32:59 +0300

    use generic inputs & dictreader

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 
 rm -rf *.csv
 wget https://radioid.net/static/user.csv -O bm-hams.csv
-wget https://s3.qra-team.online/Devices_Contact_List/Contacts.csv -O tmp.csv
-tail -n +2 tmp.csv | sed 's/""""//g' > qra-hams.csv
-rm tmp.csv
-python3 merger.py qra-hams.csv bm-hams.csv user.csv
+wget https://raw.githubusercontent.com/krot4u/Public_scripts/master/dmrid.dat -O qra-hams.csv
+python3 merger.py qra-hams.csv bm-hams.csv user.csv 250
