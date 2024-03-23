4fcec4dabe49ecb3dc784e2ca27521eb395524fd
Ivan Kulikov <kulikovis91@gmail.com>
2024-03-23 14:40:38 +0300

    opengd77 format

--- merger.py
+++ merger.py
@@ -20,11 +20,9 @@ with open(file1, "r", newline='') as csvfile:
     rdr = csv.reader(csvfile, delimiter=',')
     for row in rdr:
         data = {}
-        data['id'] = row[0]
-        data['callsign'] = row[1]
-        data['name'] = row[2]
-        data['loc'] = row[5]
-        file1data[row[0]] = data
+        data['id'] = row[1]
+        data['callsign'] = row[0]
+        file1data[row[1]] = data
 
 print(f"total members in {file1}: {len(file1data)}")
 print(f"reading second file: {file2}")
@@ -32,7 +30,8 @@ print(f"reading second file: {file2}")
 with open(file2, "r", newline='') as csvfile:
     with open(fileOut, "w", newline='') as outfile:
         rdr = csv.reader(csvfile, delimiter=',')
-        writer = csv.writer(outfile, delimiter=',')
+        writer = csv.writer(outfile, delimiter=';')
+        writer.writerow(['Contact Name', 'ID', 'ID Type' ,'TS Override'])
         for row in rdr:
             if len(prefix) > 0 and not row[5].startswith(prefix):
                 continue
@@ -43,13 +42,14 @@ with open(file2, "r", newline='') as csvfile:
 
             if id in file1data:
                 print(f"match found {row} / {file1data[id]}")
-                row1 = [id, f"{callsign} ({file1data[id]['callsign']})", name, "" , "", file1data[id]["loc"]]
+                row1 = [f"{callsign}/{file1data[id]['callsign']}", id, 'Private', 2]
                 writer.writerow(row1)
                 file1data.pop(row[0])
             else:
-               writer.writerow(row)
+               row2 = [f"{row[1]}/{row[2]}", row[0], 'Private', 2]
+               writer.writerow(row2)
 
         print(f"{file1} members left unmatched: {len(file1data)} adding them as is")
         for _, el in file1data.items():
-            row1 = [el["id"], el["callsign"], el["name"], "" , "", el["loc"]]
+            row1 = [el["callsign"], el['id'], 'Private', 2]
             writer.writerow(row1)
