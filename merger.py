29c139d44705c3dfc944cde00a963df5fc1f2598
Ivan Kulikov <kulikovis91@gmail.com>
2024-03-23 19:40:49 +0300

    formatting added

--- merger.py
+++ merger.py
@@ -13,46 +13,55 @@ if len(sys.argv) > 4:
     dmrIdPrefix = sys.argv[4]
     print(f"dmrIdPrefix {dmrIdPrefix}")
 
-file1data = {}
+csvHeader = "RADIO_ID,CALLSIGN,FIRST_NAME,LAST_NAME,CITY,STATE,COUNTRY"
+csvFormat = csvHeader.split(",")
 
-print(f"reading first file: {file1}")
-with open(file1, "r", newline='') as csvfile:
-    rdr = csv.reader(csvfile, delimiter=';')
-    for row in rdr:
-        data = {}
-        data['id'] = row[0]
-        data['callsign'] = row[1]
-        file1data[row[0]] = data
+def printRow(writer, data):
+    rowOut = []
+    for key in csvFormat:
+        if key in data:
+            rowOut.append(data[key])
+        else:
+            rowOut.append('')
+    writer.writerow(rowOut)
 
-print(f"total members in {file1}: {len(file1data)}")
-print(f"reading second file: {file2}")
+def main():
+    file1data = {}
 
-with open(file2, "r", newline='') as csvfile:
-    with open(fileOut, "w", newline='') as outfile:
-        rdr = csv.DictReader(csvfile, delimiter=',')
-        writer = csv.writer(outfile, delimiter=',')
-        writer.writerow(["RADIO_ID","CALLSIGN","FIRST_NAME","LAST_NAME","CITY","STATE","COUNTRY"])
+    print(f"reading first file: {file1}")
+    with open(file1, "r", newline='') as csvfile:
+        rdr = csv.reader(csvfile, delimiter=';')
         for row in rdr:
-            if len(dmrIdPrefix) > 0 and not row['RADIO_ID'].startswith(dmrIdPrefix):
-                continue
+            data = {}
+            data['RADIO_ID'] = row[0]
+            data['CALLSIGN'] = row[1]
+            data['COUNTRY'] = 'Russia'
+            file1data[row[0]] = data
 
-            id = row["RADIO_ID"]
-            callsign = row["CALLSIGN"]
-            fname = row["FIRST_NAME"]
-            lname = row["LAST_NAME"]
-            city = row["CITY"]
-            state = row["STATE"]
-            country = row["COUNTRY"]
+    print(f"total members in {file1}: {len(file1data)}")
+    print(f"reading second file: {file2}")
 
-            if id in file1data:
-                print(f"match found {row} <<->> {file1data[id]}")
-                row1 = [id, f"{callsign}/{file1data[id]['callsign']}", fname, lname, city, state, country]
-                writer.writerow(row1)
-                file1data.pop(row["RADIO_ID"])
-            else:
-               writer.writerow(row.values())
+    with open(file2, "r", newline='') as csvfile:
+        with open(fileOut, "w", newline='') as outfile:
+            rdr = csv.DictReader(csvfile, delimiter=',')
+            writer = csv.writer(outfile, delimiter=',')
+            writer.writerow(csvFormat)
+            for row in rdr:
+                id = row['RADIO_ID']
 
-        print(f"{file1} members left unmatched: {len(file1data)} adding them as is")
-        for _, el in file1data.items():
-            row1 = [el['id'], el["callsign"], '', '', '', '', 'Russia']
-            writer.writerow(row1)
+                if len(dmrIdPrefix) > 0 and not id.startswith(dmrIdPrefix):
+                    continue
+
+                if id in file1data:
+                    print(f"match found {row} <<->> {file1data[id]}")
+                    row["CALLSIGN"] = f"{row['CALLSIGN']}/{file1data[id]['CALLSIGN']}"
+                    file1data.pop(row["RADIO_ID"])
+
+                printRow(writer, row)
+
+            print(f"{file1} members left unmatched: {len(file1data)} adding them as is")
+            for _, el in file1data.items():
+                printRow(writer, el)
+ 
+if __name__ == "__main__":
+    main()
